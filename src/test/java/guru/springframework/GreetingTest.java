3726e9ec54a7edd82431c6e37a8ff5f73f98143f
Daniel Martínez <aprendejava73@gmail.com>
2023-03-25 08:35:43 +0100

    39. Initalizing Properties with Junit5 @BeforeEach

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,20 +1,24 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreetingTest {
+    private Greeting greeting;
+
+    @BeforeEach
+    void setUp() {
+        greeting = new Greeting();
+    }
 
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
-
         System.out.println(greeting.helloWorld());
-
     }
 
     @Test
     void helloWorld1() {
-        Greeting greeting = new Greeting();
+        greeting = new Greeting();
 
         System.out.println(greeting.helloWorld("John"));
     }
